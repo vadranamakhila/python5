f421f1e3ac59300f3e5ba36ffd84c84d4c72a433
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-12-10 12:46:50 +0530

    Update 26.py

--- 26.py
+++ 26.py
@@ -1,14 +1,14 @@
 def partition(arr,low,high):
-	j = ( low-1 )		 
+	i = ( low-1 )		 
 	pivot = arr[high]	 
 
-	for k in range(low , high):
-		if arr[k] <= pivot:
-			j = j+1
-			arr[j],arr[k] = arr[k],arr[j]
+	for j in range(low , high):
+		if arr[j] <= pivot:
+			i = i+1
+			arr[i],arr[j] = arr[j],arr[i]
 
-	arr[j+1],arr[high] = arr[high],arr[j+1]
-	return ( j+1 )
+	arr[i+1],arr[high] = arr[high],arr[i+1]
+	return ( i+1 )
 def quickSort(arr,low,high):
 	if low < high:
 		pi = partition(arr,low,high)
