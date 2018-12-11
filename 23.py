6866388da18eae63fdc7e8a2a9d4911f9aac7902
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-12-11 11:58:02 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,11 +1,11 @@
 min=int(raw_input())
-def smallest(arr,r): 
+def smallest(arr,s): 
     min = arr[0]
-    for i in range(1, r): 
-        if arr[i] > max: 
-            min = arr[i] 
+    for j in range(1, s): 
+        if arr[j] > max: 
+            min = arr[j] 
     return min
 arr = [1,2,3,4,5] 
-r = len(arr) 
-Ans = smallest(arr,r) 
+s = len(arr) 
+Ans = smallest(arr,s) 
 print (Ans) 
